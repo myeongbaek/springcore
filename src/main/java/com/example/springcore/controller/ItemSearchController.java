7eab1aef5e1185de2f4819c3608baa6c48dfb5f7
myeongbaek <moungbak421@daum.net>
2023-04-13 22:34:17 +0900

    ItemSearchController 수정

--- src/main/java/com/example/springcore/controller/ItemSearchController.java
+++ src/main/java/com/example/springcore/controller/ItemSearchController.java
@@ -32,11 +32,6 @@ public class ItemSearchController {
 
     private final SearchService searchService;
 
-    // Controller 가 자동으로 해주는 일
-    // 1. API Request 의 파라미터 값에서 검색어 추출 -> query 변수
-    // 5. API Response 보내기
-    // 5.1) response 의 header 설정
-    // 5.2) response 의 body 설정
     @GetMapping("/api/search")
     @ResponseBody
     public List<ItemDto> getItems(@RequestParam String query) throws IOException {
