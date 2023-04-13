343a3fd73d13a5219b940878eb19af004dce4c7d
myeongbaek <moungbak421@daum.net>
2023-04-13 22:48:45 +0900

    스프링 Security 추가

--- src/main/java/com/example/springcore/controller/ItemSearchController.java
+++ src/main/java/com/example/springcore/controller/ItemSearchController.java
@@ -1,31 +1,15 @@
 package com.example.springcore.controller;
 
 import com.example.springcore.dto.ItemDto;
-<<<<<<< HEAD
 import com.example.springcore.service.SearchService;
-=======
->>>>>>> origin/master
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
-=======
->>>>>>> origin/master
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
-
 @RequiredArgsConstructor
 @Controller
 public class ItemSearchController {
