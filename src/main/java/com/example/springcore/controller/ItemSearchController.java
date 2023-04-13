f5c6799c6cec3c985358f4df5348804540cae11e
myeongbaek <moungbak421@daum.net>
2023-04-13 13:02:17 +0900

    My First Commit

---
+++ src/main/java/com/example/springcore/controller/ItemSearchController.java
@@ -0,0 +1,39 @@
+package com.example.springcore.controller;
+
+import com.example.springcore.dto.ItemDto;
+import com.example.springcore.service.SearchService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ItemSearchController {
+
+    private final SearchService searchService;
+
+    // Controller 가 자동으로 해주는 일
+    // 1. API Request 의 파라미터 값에서 검색어 추출 -> query 변수
+    // 5. API Response 보내기
+    // 5.1) response 의 header 설정
+    // 5.2) response 의 body 설정
+    @GetMapping("/api/search")
+    @ResponseBody
+    public List<ItemDto> getItems(@RequestParam String query) throws IOException {
+        return searchService.searchItems(query);
+    }
+}
---
+++ src/main/java/com/example/springcore/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.springcore.repository;
+
+import com.example.springcore.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> { }
